89c91fa6c08bb73620008991e2ab3d36359e868b
Andrey Kitelev <kitelev.andrey@gmail.com>
2025-11-03 16:16:03 +0500

    Initial Commit

---
+++ my-first-ios-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  my-first-ios-app
+//
+//  Created by Андрей Кителёв on 03.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ my-first-ios-app/my_first_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_first_ios_appApp.swift
+//  my-first-ios-app
+//
+//  Created by Андрей Кителёв on 03.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct my_first_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
